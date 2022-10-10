2fe4ae985be692565ba3ddf5de10445667658504
lakmal34 <lakmalkaviratne@gmail.com>
2022-10-11 01:10:18 +0530

    add responsive layouts add darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { Inject, Injectable} from '@angular/core';
+import { DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,15 @@ import {MenuItem} from 'primeng/api';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  dark: boolean = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document){}
+  switchTheme(theme: string) {
+    let themeLink = this.document.getElementById('app-theme')as HTMLLinkElement;
+    if (themeLink) {
+        themeLink.href = theme + 'css';
+    }
+  }
   
 }
